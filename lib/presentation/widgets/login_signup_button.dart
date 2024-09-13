21f20b41a33b674bc94f392f9d74319548db3177
FolaDev <folarinfadeyi@gmail.com>
2024-09-14 00:13:20 +0100

    changes

--- lib/presentation/widgets/login_signup_button.dart
+++ lib/presentation/widgets/login_signup_button.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:project_todo/constants/colors/colors.dart';
 
 class LoginSignUpButton extends StatelessWidget {
-  const LoginSignUpButton({super.key, required this.text});
+  const LoginSignUpButton(
+      {super.key, required this.text, required this.onPressed});
   final String text;
-
+  final VoidCallback onPressed;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -17,7 +18,7 @@ class LoginSignUpButton extends StatelessWidget {
           borderRadius: BorderRadius.circular(20),
         ),
         child: TextButton(
-          onPressed: () {},
+          onPressed: onPressed,
           child: Text(
             text,
             style: const TextStyle(
