22629dc28cffcb29baaad039df557fbfebc06a88
FolaDev <folarinfadeyi@gmail.com>
2024-09-19 22:35:10 +0100

    initializing firebase auth for login and sign up

--- lib/auth/firebase_auth.dart
+++ lib/auth/firebase_auth.dart
@@ -1,15 +1,43 @@
-abstract class FirebaseAuth {
-  Future<void> registerUser(
-      String email, String password, String confirmPassword);
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
-  Future<void> logIn(String email, String password);
+abstract class FirebaseAuthDataSource {
+  Future<void> registerUser(
+    String email,
+    String password,
+    String confirmPassword,
+  );
+  Future<void> logIn(
+    String email,
+    String password,
+  );
 }
 
-class AuthRemote extends FirebaseAuth {
+class AuthRemote extends FirebaseAuthDataSource {
   @override
   Future<void> registerUser(
-      String email, String password, String confirmPassword) async {}
+      String email, String password, String confirmPassword) async {
+    try {
+      if (confirmPassword == password) {
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+      }
+    } catch (e) {
+      Text(e.toString());
+    }
+  }
 
   @override
-  Future<void> logIn(String email, String password) async {}
+  Future<void> logIn(String email, String password) async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } catch (e) {
+      Text(e.toString());
+    }
+  }
 }
