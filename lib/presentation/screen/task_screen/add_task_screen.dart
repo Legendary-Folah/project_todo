029711ad0bd49c01074a810ad171e532cd0a7948
FolaDev <folarinfadeyi@gmail.com>
2024-10-10 20:27:53 +0100

    changes

--- lib/presentation/screen/task_screen/add_task_screen.dart
+++ lib/presentation/screen/task_screen/add_task_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_todo/constants/colors/colors.dart';
 import 'package:project_todo/presentation/widgets/add_task_text_field.dart';
 
 class AddTaskScreen extends StatefulWidget {
@@ -14,6 +15,8 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
 
   final focusNode1 = FocusNode();
   final focusNode2 = FocusNode();
+
+  int indexx = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,6 +36,42 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
             obscureText: false,
             maxLines: 3,
           ),
+          const SizedBox(height: 18),
+          Container(
+            height: 150,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: "assets/images/".length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        indexx = index;
+                      });
+                    },
+                    child: Container(
+                      width: 180,
+                      margin: const EdgeInsets.symmetric(horizontal: 10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        border: Border.all(
+                          color: indexx == index
+                              ? ColorsConst.customGreen
+                              : ColorsConst.grey,
+                          width: 2.0,
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            "assets/images/$index.jpg",
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }),
+          )
         ],
       )),
     );
