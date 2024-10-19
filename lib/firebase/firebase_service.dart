8b8041b66c75765eb5d1b6e813307c8fd1326b5f
FolaDev <folarinfadeyi@gmail.com>
2024-10-19 12:20:01 +0100

    change

--- lib/firebase/firebase_service.dart
+++ lib/firebase/firebase_service.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:uuid/uuid.dart';
 
 class FirestoreDataSource {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
@@ -12,6 +13,17 @@ class FirestoreDataSource {
           .doc(_auth.currentUser!.uid)
           .set({'id': _auth.currentUser!.uid, "email": email});
       print('Email: $email');
+      return true;
+    } catch (e) {
+      return true;
+    }
+  }
+
+  Future<bool?> addNote(String title, String subTitle, int image) async {
+    try {
+      final uuid = const Uuid().v4();
+      final date = DateTime.now();
+
       return true;
     } catch (e) {
       return true;
